ffc0b88d6383feae93064e1c88f2a90e136a16c0
Dinuka <dinukanh@gmail.com>
2024-06-17 22:54:28 +1000

    GET a joke by filtering for joke type

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ app.get("/jokes/:id", (req, res) => {
 })
 
 //3. GET a jokes by filtering on the joke type
+app.get("/filter", (req, res) => {
+  const type = req.query.type;
+  const filteredType = jokes.filter((joke) => joke.joketype === type);
+  res.json(filteredType);
+})
 
 //4. POST a new joke
 
